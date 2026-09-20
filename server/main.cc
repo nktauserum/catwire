09d82538db990866c0bcff0a3250861f12be73d6
nktauserum <auserum@proton.me>
2026-09-20 12:30:25 +0300

    Remove subclasses

--- server/main.cc
+++ server/main.cc
@@ -9,7 +9,9 @@
 
 #define BUF_COUNT 256
 #define BUF_SIZE 65535+16
+#define UDP_PORT 45230
 
+#define BUF_OFFSET(base, idx) (reinterpret_cast<unsigned char*>(base) + BUF_SIZE*idx)
 
 class BufferRing {
 public:
@@ -50,17 +52,27 @@ public:
     }
 };
 
-class Ring {
-public:
+class UDP {
+private:
     struct io_uring ring;
-    BufferRing* buffer_ring;
+    struct msghdr msg;
+    int fd = 0;
+
+    struct io_uring_buf_ring* buf_ring;
+    void* base;
+    size_t map_size;
+
+    void recycle(unsigned int idx) {
+        io_uring_buf_ring_add(buf_ring, BUF_OFFSET(base, idx), BUF_SIZE, idx, io_uring_buf_ring_mask(BUF_COUNT), 0);
+        io_uring_buf_ring_advance(buf_ring, 1);
+    }
 
     bool get_sqe(struct io_uring_sqe** sqe) {
         *sqe = io_uring_get_sqe(&ring);
 
         if (!*sqe) {
             io_uring_submit(&ring);
-            *sqe = io_uring_get_sqe(&ring);
+            *sqe = io_uring_get_sqe(&ring);    
         }
         if (!*sqe) {
             fprintf(stderr, "cannot get sqe\n");
@@ -69,57 +81,18 @@ public:
         return false;
     }
 
-    bool setup(BufferRing* br) {
-        this->buffer_ring = std::move(br);
-        unsigned int buffer_count = buffer_ring->count;
-
-        struct io_uring_params params;
-        memset(&params, 0, sizeof(params));
-        memset(&ring, 0, sizeof(ring));
-
-        params.cq_entries = buffer_count * 2;
-        params.flags = IORING_SETUP_SUBMIT_ALL | IORING_SETUP_COOP_TASKRUN | IORING_SETUP_CQSIZE;
-
-        int ret = io_uring_queue_init_params(buffer_count, &ring, &params);
-        if (ret < 0) {
-            fprintf(stderr, "queue_init failed: %s\n", strerror(-ret));
-            return false;
-        }
-
-        return true;
-    }
-
-    bool register_fd(int fd) {
-        int ret = io_uring_register_files(&ring, &fd, 1);
-        if (ret) {
-            fprintf(stderr, "register files: %s\n", strerror(-ret));
-            return false;
-        }
-
-        return true;
-    }
-};
-
-class Socket {
-private:
-    int port = 0;
-    Ring ring = Ring();
-    BufferRing buffer_ring = BufferRing(BUF_COUNT, BUF_SIZE);
-    struct msghdr msg;
 
 public:
-    int fd = 0;
-
-    bool open() {
+    int open() {
         int fd = socket(AF_INET, SOCK_DGRAM, 0); // only ipv4 is supported
         if (fd < 0) {
             fprintf(stderr, "sock_init: %s\n", strerror(errno));
-            return false;
+            return -1;
         }
 
         struct sockaddr_in addr = {
             .sin_family = AF_INET,
-            .sin_port = htons(port),
+            .sin_port = htons(UDP_PORT),
             .sin_addr = { INADDR_ANY }
         };
 
@@ -127,16 +100,15 @@ public:
         if (ret) {
             fprintf(stderr, "sock_bind: %s\n", strerror(errno));
             close(fd);
-            return false;
+            return -1;
         }
 
-        this->fd = fd;
-        return true;
+        return fd;
     }
 
     bool add_recv() {
         struct io_uring_sqe *sqe;
-        if (ring.get_sqe(&sqe))
+        if (get_sqe(&sqe))
             return false;
 
         io_uring_prep_recvmsg_multishot(sqe, 0, &msg, MSG_TRUNC);
@@ -144,32 +116,51 @@ public:
         sqe->flags |= IOSQE_FIXED_FILE;
         sqe->flags |= IOSQE_BUFFER_SELECT;
         sqe->buf_group = 0;
-        io_uring_sqe_set_data64(sqe, buffer_ring.count + 1);
+        io_uring_sqe_set_data64(sqe, BUF_COUNT + 1);
 
         return true;
     }
 
     bool setup() {
-        if (!open())
+        fd = open();
+        if (fd != 0) 
             return false;
 
-        if (!buffer_ring.setup()) {
-            fprintf(stderr, "buffer_ring setup failed");
-            io_uring_queue_exit(&ring.ring);
+        // init io_uring
+        struct io_uring_params params;
+        memset(&params, 0, sizeof(params));
+        memset(&ring, 0, sizeof(ring));
+
+        params.cq_entries = BUF_COUNT * 2;
+        params.flags = IORING_SETUP_SUBMIT_ALL | IORING_SETUP_COOP_TASKRUN | IORING_SETUP_CQSIZE;
+
+        int ret = io_uring_queue_init_params(BUF_COUNT, &ring, &params);
+        if (ret < 0) {
+            fprintf(stderr, "queue_init failed: %s\n", strerror(-ret));
             return false;
         }
 
-        if (!ring.setup(&buffer_ring))
+        // init buffer ring
+        map_size = (sizeof(struct io_uring_buf) + BUF_SIZE) * BUF_COUNT;
+        void* mapped = mmap(NULL, map_size, PROT_READ | PROT_WRITE,
+                  MAP_ANONYMOUS | MAP_PRIVATE, 0, 0);
+        if (mapped == MAP_FAILED) {
+            fprintf(stderr, "buf_ring mmap: %s\n", strerror(errno));
             return false;
+        }
+        buf_ring = (struct io_uring_buf_ring *)mapped;
+        io_uring_buf_ring_init(buf_ring);
+
+        base = (unsigned char*)mapped + BUF_COUNT * sizeof(struct io_uring_buf);
 
 
         struct io_uring_buf_reg reg = { 
-            .ring_addr = reinterpret_cast<unsigned long>(buffer_ring.buf_ring),
-            .ring_entries = static_cast<unsigned int>(buffer_ring.count),
+            .ring_addr = reinterpret_cast<unsigned long>(buf_ring),
+            .ring_entries = BUF_COUNT,
             .bgid = 0
         };
 
-        int ret = io_uring_register_buf_ring(&ring.ring, &reg, 0);
+        ret = io_uring_register_buf_ring(&ring, &reg, 0);
         if (ret) {
             fprintf(stderr, "buf_ring init failed: %s\n"
                     "NB This requires a kernel version >= 6.0\n",
@@ -177,17 +168,17 @@ public:
             return ret;
         }
 
-        for (int i = 0; i < buffer_ring.count; i++) {
-            io_uring_buf_ring_add(buffer_ring.buf_ring, buffer_ring.extract(i), buffer_ring.size, i,
-                          io_uring_buf_ring_mask(buffer_ring.count), i);
+        for (int i = 0; i < BUF_COUNT; i++) {
+            io_uring_buf_ring_add(buf_ring, BUF_OFFSET(base, i), BUF_SIZE, i,
+                          io_uring_buf_ring_mask(BUF_COUNT), i);
         }
-        io_uring_buf_ring_advance(buffer_ring.buf_ring, buffer_ring.count);
+        io_uring_buf_ring_advance(buf_ring, BUF_COUNT);
 
         memset(&msg, 0, sizeof(msg));
         msg.msg_namelen = sizeof(struct sockaddr_storage);
         msg.msg_controllen = 0;
 
-        ret = io_uring_register_files(&ring.ring, &fd, 1);
+        ret = io_uring_register_files(&ring, &fd, 1);
         if (ret) {
             fprintf(stderr, "register files: %s\n", strerror(-ret));
             return false;
@@ -199,7 +190,7 @@ public:
     void listen() {
         struct io_uring_cqe *cqes[BUF_COUNT*2];
         while (true) {
-            int ret = io_uring_submit_and_wait(&ring.ring, 1);
+            int ret = io_uring_submit_and_wait(&ring, 1);
             if (ret == -EINTR)
                 continue;
             if (ret < 0) {
@@ -207,7 +198,7 @@ public:
                 break;
             }
 
-            int count = io_uring_peek_batch_cqe(&ring.ring, &cqes[0], BUF_COUNT*2);
+            int count = io_uring_peek_batch_cqe(&ring, &cqes[0], BUF_COUNT*2);
             for (int i = 0; i < count; i++) {
                 struct io_uring_cqe *cqe = cqes[i];
                 int idx = cqe->flags >> 16;
@@ -222,9 +213,9 @@ public:
                         continue;
                     }
 
-                    fprintf(stderr, "%p %d\n", buffer_ring.extract(idx), cqe->res);
+                    fprintf(stderr, "%p %d\n", BUF_OFFSET(base, idx), cqe->res);
 
-                    struct io_uring_recvmsg_out *out = io_uring_recvmsg_validate(buffer_ring.extract(idx), cqe->res, &msg);
+                    struct io_uring_recvmsg_out *out = io_uring_recvmsg_validate(BUF_OFFSET(base, idx), cqe->res, &msg);
                     if (!out) {
                         fprintf(stderr, "bad recvmsg\n");
                         continue;
@@ -234,7 +225,7 @@ public:
                         unsigned int r = io_uring_recvmsg_payload_length(out, cqe->res, &msg);
                         fprintf(stderr, "truncated msg need %u received %u\n",
                                 out->payloadlen, r);
-                        buffer_ring.recycle(idx);
+                        recycle(idx);
                         continue;
                     }
 
@@ -251,26 +242,26 @@ public:
                         out->namelen, name, (int)ntohs(addr->sin_port));
                 }
               
-                buffer_ring.recycle(idx);
+                recycle(idx);
             }
         
-            io_uring_cq_advance(&ring.ring, count);
+            io_uring_cq_advance(&ring, count);
         }
     }
 
-    Socket(int port) : port{port} {};
-    ~Socket() {
+    ~UDP() {
         close(fd);
+        munmap(reinterpret_cast<void*>(buf_ring), map_size);
     }
 };
 
 int main(void) {
-    Socket socket = Socket(43250);
-    bool ok = socket.setup();
+    UDP udp_listener = UDP(); 
+    bool ok = udp_listener.setup();
     if (!ok) 
         return 1;
 
-    socket.listen();
+    udp_listener.listen();
 
     return 0;
 }
