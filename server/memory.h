6247858fb5ec26157645b3722bf605b47ad9c3c2
nktauserum <auserum@proton.me>
2026-09-21 11:01:20 +0300

    Register worker function

--- server/memory.h
+++ server/memory.h
@@ -86,10 +86,15 @@ template <typename T, int workers_count>
 class Channel {
 private:
     Queue<T> workers[workers_count];
+    alignas(64) int count = 0;
     alignas(64) u32 next = 0;
 
 public:
-        void push(T item) {
+    Queue<T>* add_worker() {
+        return &workers[reinterpret_cast<std::atomic<int>*>(&count)->fetch_add(1, std::memory_order_relaxed)];
+    }
+
+    void push(T item) {
         int idx = (reinterpret_cast<std::atomic<u32>*>(&next)->fetch_add(1, std::memory_order_relaxed) - 1) % workers_count;
         Queue<T>& worker = workers[idx];
 
