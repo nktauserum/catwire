e16fb171ff10cf1dda42244571c4dc4b37c0fe83
nktauserum <auserum@proton.me>
2026-09-23 12:38:17 +0300

    Parse .ini config

--- common/models/session.h
+++ common/models/session.h
@@ -6,7 +6,10 @@
 struct Session {
     u8 shared_key[32] = {0};
     u8 publicKey[32] = {0};
+    u32 local_addr;
     Address remote_addr;
-
-    std::atomic<u32> available;
 };
+
+bool compareSessionsByAddr(const Session& a, const Session& b) {
+    return a.local_addr < b.local_addr;
+}
